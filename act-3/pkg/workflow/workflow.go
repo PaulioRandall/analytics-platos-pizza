7651b52626aea34751982e3b56c9b76f7c6ef8cb
Paulio <pauliorandall@gmail.com>
2022-11-21 09:52:54 +0000

    Remove obsolete workflow pkg file

--- act-3/pkg/workflow/workflow.go
+++ act-3/pkg/workflow/workflow.go
@@ -20,3 +20,32 @@ func Execute() error {
 
 	return nil
 }
+
+func insertData(db database.PlatosPizzaDatabase) error {
+	e := database.InsertMetadata(db, "../data/data_dictionary.csv")
+	if e != nil {
+		return err.Wrap(e, "Failed to insert metadata")
+	}
+
+	e = database.InsertOrders(db, "../data/orders.csv")
+	if e != nil {
+		return err.Wrap(e, "Failed to insert orders")
+	}
+
+	e = database.InsertOrderDetails(db, "../data/order_details.csv")
+	if e != nil {
+		return err.Wrap(e, "Failed to insert order details")
+	}
+
+	e = database.InsertPizzas(db, "../data/pizzas.csv")
+	if e != nil {
+		return err.Wrap(e, "Failed to insert pizzas")
+	}
+
+	e = database.InsertPizzaTypes(db, "../data/pizza_types.csv")
+	if e != nil {
+		return err.Wrap(e, "Failed to insert pizza types")
+	}
+
+	return nil
+}
